74bdcbc932634867b7c40c4dfce06a86a4c59e52
chrisyan82000 <chrisyan82000@yahoo.com>
2021-03-03 10:14:53 -0800

    Create 236.py

---
+++ 236.py
@@ -0,0 +1,7 @@
+if not root or p == root or q == root:
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if left and right:
+            return root
+        return left if left else right
