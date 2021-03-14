1c9a8184d43cf6f40c976df1af2fe4e8fb6765fe
chrisyan82000 <chrisyan82000@yahoo.com>
2021-03-13 18:31:54 -0800

    Create 986.py

---
+++ 986.py
@@ -0,0 +1,14 @@
+def intervalIntersection(self, A: List[List[int]], B: List[List[int]]) -> List[List[int]]:
+        i, j, res = 0, 0, []
+        while i < len(A) and j < len(B):
+            if A[i][1] < B[j][0]:
+                i += 1
+            elif B[j][1] < A[i][0]:
+                j += 1
+            else:
+                res.append([max(A[i][0], B[j][0]), min(A[i][1], B[j][1])]) 
+                if A[i][1] < B[j][1]:
+                    i += 1
+                else:
+                    j += 1
+        return res
