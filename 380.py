e0c1d16dbf5aa17c8028455ccd4c764345e22c1c
chrisyan82000 <chrisyan82000@yahoo.com>
2021-04-19 17:35:24 -0700

    Create 380.py

---
+++ 380.py
@@ -0,0 +1,54 @@
+from random import choice
+
+class RandomizedSet:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+
+        self.hash_map = {}
+        self.values = []
+
+    def insert(self, val):
+        """
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
+        :type val: int
+        :rtype: bool
+        """
+
+        if val not in self.hash_map:
+            self.hash_map[val] = len(self.values)
+            self.values.append(val)
+
+            return True
+
+        return False
+
+    def remove(self, val):
+        """
+        Removes a value from the set. Returns true if the set contained the specified element.
+        :type val: int
+        :rtype: bool
+        """
+
+        if val in self.hash_map:
+            index = self.hash_map[val]
+            last_val = self.values.pop()
+            if index < len(self.values):
+                self.values[index] = last_val
+                self.hash_map[last_val] = index
+            del self.hash_map[val]
+
+            return True
+
+        return False
+
+    def getRandom(self):
+        """
+        Get a random element from the set.
+        :rtype: int
+        """
+
+        return choice(self.values)
+
