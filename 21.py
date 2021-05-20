e119730428835e8df1a250191969a87bbe8c3f76
chrisyan82000 <chrisyan82000@yahoo.com>
2021-05-20 15:14:54 -0700

    Create 21.py

---
+++ 21.py
@@ -0,0 +1,16 @@
+class ListNode:
+# #     def __init__(self, x):
+# #         self.val = x
+# #         self.next = None
+# # def mergeTwoLists(l1,l2):
+# #     dummy = curr = ListNode(0)
+# #     while l1 and l2:
+# #         if l1.val < l2.val:
+# #             curr.next = l1
+# #             l1 = l1.next
+# #         else:
+# #             curr.next = l2
+# #             l2 = l2.next
+# #         curr = curr.next    
+# #     curr.next = l1 or l2        
+# #     return dummy.next 
