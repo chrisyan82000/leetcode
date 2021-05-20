580c189817a79f617b22b26c360a58e5f8d34fdb
chrisyan82000 <chrisyan82000@yahoo.com>
2021-05-20 15:24:48 -0700

    Create 69.py

---
+++ 69.py
@@ -0,0 +1,12 @@
+def squareRoot(x):
+# #     start = 0
+# #     end = x
+# #     while start <= end:
+# #         mid = start + (end - start) // 2
+# #         if mid**2 == x:
+# #             return mid
+# #         elif mid**2 < x:
+# #             start = mid + 1
+# #         else:
+# #             end = mid -1
+# #     return end    
