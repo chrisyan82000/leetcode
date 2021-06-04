a08d122eaee485675ad6ac2ace29ee2d5fef71a9
chrisyan82000 <chrisyan82000@yahoo.com>
2021-06-04 12:29:13 -0700

    Create 697.py

---
+++ 697.py
@@ -0,0 +1,14 @@
+def degree(nums):
+#     count = {}
+#     min_arr = {}
+#     max_deg = {}
+    
+#     for i, num in enumerate(nums):
+#         if num in count:
+#             count[num] += 1
+#         else:
+#             count[num] = 1
+#             min_arr[num] = i 
+#         max_deg[num] = i 
+#     return min(max_deg[num] - min_arr[num] + 1 for num, freq in count.items() if freq == max(count.values()))
+
