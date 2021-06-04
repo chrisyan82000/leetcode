587d8e11ba2244b8f239f9ab144915f74053cb3a
chrisyan82000 <chrisyan82000@yahoo.com>
2021-06-04 12:33:18 -0700

    Create 905.py

---
+++ 905.py
@@ -0,0 +1,10 @@
+def sortParity(nums): 
+# #     i = 0
+# #     j = len(nums) -1
+# #     #while i < j:
+# #     while nums[i] % 2 == 0 and i < j:
+# #             i += 1
+# #     while nums[j] % 2 == 1 and i < j:
+# #             j -= 1
+# #     nums[i], nums[j] = nums[j], nums[i]
+# #     return nums
