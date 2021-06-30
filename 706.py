8f1cadd58e5fafb56d33ad9ec5c61408723dcc7a
chrisyan82000 <chrisyan82000@yahoo.com>
2021-06-29 21:18:41 -0700

    Create 706.py

---
+++ 706.py
@@ -0,0 +1,35 @@
+def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.hash_table = [-1]*1000001
+
+
+    def put(self, key, value):
+        """
+        value will always be non-negative.
+        :type key: int
+        :type value: int
+        :rtype: None
+        """
+        self.hash_table[key] = value
+
+    def get(self, key):
+        """
+        Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
+        :type key: int
+        :rtype: int
+        """
+        return self.hash_table[key]
+  
+       
+        
+
+    def remove(self, key):
+        """
+        Removes the mapping of the specified value key if this map contains a mapping for the key
+        :type key: int
+        :rtype: None
+        """
+        self.hash_table[key] = -1
+
